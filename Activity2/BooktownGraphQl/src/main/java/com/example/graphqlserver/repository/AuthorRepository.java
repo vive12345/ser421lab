47f42005fa3fe93e708174ffa44cf19d1db77433
vive12345 <vipsakamani141100@gmail.com>
2025-06-27 18:59:41 -0700

    acitivity 2 added task 1

---
+++ Activity2/BooktownGraphQl/src/main/java/com/example/graphqlserver/repository/AuthorRepository.java
@@ -0,0 +1,44 @@
+package com.example.graphqlserver.repository;
+
+import com.example.graphqlserver.model.Author;
+import com.example.graphqlserver.model.Book;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+@Repository
+public class AuthorRepository {
+    private static ArrayList<Author> dummyAuthors = new ArrayList<>();
+
+    static {
+        Author author1 = new Author(0, "Robert", "Frost", BookRepository.getBooksByAuthorId(0));
+        Author author2 = new Author(1, "Martin", "Fowler", BookRepository.getBooksByAuthorId(1));
+        Author author3 = new Author(2, "Kevin", "Gary", BookRepository.getBooksByAuthorId(2));
+
+        dummyAuthors.addAll(Arrays.asList(author1, author2, author3));
+    }
+
+    public List<Author> getAuthors() {
+        return dummyAuthors;
+    }
+
+    public Author getAuthorById(int id) {
+        for (Author author : dummyAuthors) {
+            if (author.getId() == id) {
+                return author;
+            }
+        }
+        return null;
+    }
+
+    public Author save(String firstName, String lastName) {
+        List<Book> book = new ArrayList<>();
+        int nextId = dummyAuthors.isEmpty() ? 0 : dummyAuthors.get(dummyAuthors.size() - 1).getId() + 1;
+        Author newAuthor = new Author(nextId, firstName, lastName, book);
+        dummyAuthors.add(newAuthor);
+        return newAuthor;
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+## Acitivity 1
+
 ![alt text](image.png)
 ![alt text](image-1.png)
 ![alt text](image-2.png)
+
+## Acitivity 2
+
+![alt text](image-3.png)
